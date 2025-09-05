58d79cca110fe2f02c31729f9b996895b4a93e1a
gardnervile <lordy1jo@gmail.com>
2025-09-05 11:58:39 +0000

    update deploy script with rollbar deploy notification

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -1,6 +1,22 @@
-echo "Отправляем информацию о деплое в Rollbar..."
-curl -s https://api.rollbar.com/api/1/deploy/ \
+#!/bin/bash
+set -e
+
+cd /opt/star-burger
+source venv/bin/activate
+export $(grep -v '^#' .env | xargs)   # <-- вот это важно
+
+git pull
+pip install -r requirements.txt
+npm install
+npm run build
+python manage.py collectstatic --noinput
+python manage.py migrate
+
+sudo systemctl restart gunicorn
+sudo systemctl reload nginx
+
+# Отправляем инфу в Rollbar
+curl https://api.rollbar.com/api/1/deploy/ \
   -F access_token=$ROLLBAR_DEPLOY_TOKEN \
-  -F environment=production \
-  -F revision=$(git rev-parse HEAD) > /dev/null
-echo " Деплой завершён и зафиксирован в Rollbar."
+  -F environment=$ROLLBAR_ENVIRONMENT \
+  -F revision=$(git rev-parse HEAD)
